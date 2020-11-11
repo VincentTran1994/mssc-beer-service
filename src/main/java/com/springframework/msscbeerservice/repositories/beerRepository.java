5903b77b762636c4d13016c1bc0bbce488075347
VincentTran1994 <trancongvuit@gmail.com>
2020-11-10 21:15:16 -0500

    add beer respository

---
+++ src/main/java/com/springframework/msscbeerservice/repositories/beerRepository.java
@@ -0,0 +1,6 @@
+package com.springframework.msscbeerservice.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface beerRepository extends PagingAndSortingRepository {
+}
